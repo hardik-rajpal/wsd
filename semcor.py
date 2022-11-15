afb327a9321ccb06f72fe79922ca029d88424dcb
Hardik <hardikrajpal@cse.iitb.ac.in>
2022-11-15 17:28:02 +0530

    preprocessed data

--- semcor.py
+++ semcor.py
@@ -1,20 +1,39 @@
+import json
 from typing import List
+
 from nltk.corpus import semcor
 from nltk.corpus.reader import SemcorCorpusReader
+from nltk.tag.mapping import tagset_mapping
 from nltk.tree import Tree
+
 corpus:SemcorCorpusReader = semcor
 # sents1 = list(corpus.chunk_sents())
-sents = list(corpus.tagged_sents(fileids=[corpus._fileids[0]],tag="both"))[:2]
-for i in range(len(sents)):
-    s = sents[i]
-    for tk in s:
-        tk:Tree
-        print(tk,tk.label(),tk.draw(),tk.leaves())
-    exit()
-# print(sents[:2])
-# s:List[Tree] = sents[0]
-# trees = list(filter(lambda x:str(type(x)).split("'")[1].split(".")[-1]=='Tree',s))
-# senses = list(map(lambda x:str(x.label()).split("Lemma('")[1].split("')")[0],trees))
-# # print(corpus.xml())
-# print(s[1])
-# s[1].node
+semtagwordsep = '**'
+mapping = tagset_mapping('en-ptb','universal')
+globsents = []
+for j in range(len(corpus._fileids)):
+    fileid = corpus._fileids[j]
+    sents = list(corpus.tagged_sents(fileids=[fileid],tag="both"))[:2]
+    nountag = 'NN'
+    for i in range(len(sents)):
+        s = sents[i]
+        new_sent = []
+        for tk in s:
+            tk:Tree
+            subtrees = list(tk.subtrees())
+            labels = []
+            for subtree in subtrees:
+                labels.append(subtree.label())
+            labels.append('_'.join(subtree.leaves()))
+            # print(labels)
+            if(None in labels):continue
+            if(nountag in labels):
+                new_sent.append(semtagwordsep.join([labels[-1],mapping[nountag],labels[0]]))
+            else:
+                new_sent.append(semtagwordsep.join([labels[-1],mapping[labels[-2]]]))
+        sents[i] = new_sent
+    globsents.extend(sents)
+    print(f'Done: ',round(100*j/len(corpus._fileids),2))
+fname = 'tagsemsents'
+with open(f'data/{fname}.txt','w+') as f:
+    f.write(json.dumps(globsents))
